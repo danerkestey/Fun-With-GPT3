a3bd5f9bcdad1be114180115c9d8565fd0deaabf
Daner Yasin <danerkyasin@gmail.com>
2022-05-21 01:28:19 -0400

    yeeeo

--- src/components/ResponseBox.jsx
+++ src/components/ResponseBox.jsx
@@ -5,8 +5,16 @@ const PromptContainer = Styled.div`
     
 `;
 
-const PromptTextBox = Styled.input`
-    
+
+const PromptTextBox = Styled.div`
+    height:100px;
+    width:140px;
+    overflow:auto;
+    background-color:yellowgreen;
+    color:white;
+    scrollbar-base-color:gold;
+    font-family:sans-serif;
+    padding:10px;
 
 `;
 
